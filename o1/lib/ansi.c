a750f4fcd984589c549359f111b5770ccc370084
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2025-10-14 22:24:41 -0400

    define ansi structs + color bitmask

---
+++ o1/lib/ansi.c
@@ -0,0 +1,42 @@
+//
+//  ansi.c
+//  o1
+//
+//  Created by gpt-5-high on 2025-10-12.
+//
+
+#include "ansi.h"
+
+#include <stdint.h>
+
+#define ANSI_BITWISE_MASK    0xFF000000u
+#define ANSI_BITWISE_INDEXED 0x01000000u
+#define ANSI_BITWISE_RGB     0x02000000u
+
+uint32_t ansi_color_pack_indexed(int index) {
+    return ANSI_BITWISE_INDEXED | ((uint32_t)(index) & 0xFFu);
+}
+
+uint32_t ansi_color_pack_rgb(uint8_t red, uint8_t green, uint8_t blue) {
+    return ANSI_BITWISE_RGB | (((uint32_t)red & 0xFFu) << 16) | (((uint32_t)green & 0xFFu) << 8) | ((uint32_t)blue & 0xFFu);
+}
+
+ansi_color_t ansi_color_unpack(uint32_t color, int *index, uint8_t *red, uint8_t *green, uint8_t *blue) {
+    if (color == ANSI_COLOR_RESET) return ANSI_COLOR_DEFAULT;
+
+    if ((color & ANSI_BITWISE_MASK) == ANSI_BITWISE_INDEXED) {
+        if (index) *index = color & 0xFFu;
+
+        return ANSI_COLOR_INDEXED;
+    }
+
+    if ((color & ANSI_BITWISE_MASK) == ANSI_BITWISE_RGB) {
+        if (red) *red = (color >> 16) & 0xFFu;
+        if (green) *green = (color >> 8) & 0xFFu;
+        if (blue) *blue = color & 0xFFu;
+
+        return ANSI_COLOR_RGB;
+    }
+
+    return ANSI_COLOR_DEFAULT;
+}
---
+++ o1/lib/ansi.h
@@ -0,0 +1,161 @@
+//
+//  ansi.h
+//  o1
+//
+//  Created by gpt-5-high on 2025-10-12.
+//
+
+#ifndef ANSI_H
+#define ANSI_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+#define ANSI_MAX_PARAMETERS 16
+
+#define ANSI_COLOR_RESET 0xFFFFFFFFu
+
+typedef enum ansi_event_t {
+    ANSI_EVENT_UNKNOWN = 0,
+    ANSI_EVENT_TEXT,
+    ANSI_EVENT_ESC,
+    ANSI_EVENT_CSI,
+    ANSI_EVENT_OSC,
+    ANSI_EVENT_BELL,
+} ansi_event_t;
+
+typedef struct ansi_text_t {
+    const uint8_t *bytes;
+    size_t length;
+} ansi_text_t;
+
+typedef enum ansi_esc_event_t {
+    ANSI_ESC_DEC_SAVE_CURSOR = 0,
+    ANSI_ESC_DEC_RESTORE_CURSOR,
+    ANSI_ESC_TAB_SET,
+    ANSI_ESC_IND,
+    ANSI_ESC_RI,
+    ANSI_ESC_RESET,
+} ansi_esc_event_t;
+
+typedef struct ansi_esc_t {
+    ansi_esc_event_t event;
+} ansi_esc_t;
+
+typedef enum ansi_csi_event_t {
+    ANSI_CSI_KIND_UNKNOWN = 0,
+    ANSI_CSI_CUU,
+    ANSI_CSI_CUD,
+    ANSI_CSI_CUF,
+    ANSI_CSI_CUB,
+    ANSI_CSI_CNL,
+    ANSI_CSI_CPL,
+    ANSI_CSI_CHA,
+    ANSI_CSI_CUP,
+    ANSI_CSI_HVP,
+    ANSI_CSI_ED,
+    ANSI_CSI_EL,
+    ANSI_CSI_DECSED,
+    ANSI_CSI_DECSEL,
+    ANSI_CSI_SU,
+    ANSI_CSI_SD,
+    ANSI_CSI_DECSTBM,
+    ANSI_CSI_SGR,
+    ANSI_CSI_SM,
+    ANSI_CSI_RM,
+    ANSI_CSI_DECSET,
+    ANSI_CSI_DECRST,
+    ANSI_CSI_DSR,
+    ANSI_CSI_DEC_DSR,
+    ANSI_CSI_DA,
+    ANSI_CSI_REP,
+    ANSI_CSI_TBC,
+    ANSI_CSI_BRP_START,
+    ANSI_CSI_BRP_END,
+} ansi_csi_event_t;
+
+typedef enum ansi_dec_mode_t {
+    DEC_MODE_UNKNOWN         = 0,
+    DEC_MODE_CURSOR_BLINK    = 12,
+    DEC_MODE_CURSOR_VISIBLE  = 25,
+    DEC_MODE_MOUSE_X10       = 1000,
+    DEC_MODE_MOUSE_NORMAL    = 1002,
+    DEC_MODE_MOUSE_ALL       = 1003,
+    DEC_MODE_FOCUS_REPORT    = 1004,
+    DEC_MODE_MOUSE_SGR       = 1006,
+    DEC_MODE_BRACKETED_PASTE = 2004,
+} ansi_dec_mode_t;
+
+typedef enum ansi_sgr_flag_t {
+    SGR_ATTR_NONE      = 0,
+    SGR_ATTR_BOLD      = 1u << 0,
+    SGR_ATTR_FAINT     = 1u << 1,
+    SGR_ATTR_ITALIC    = 1u << 2,
+    SGR_ATTR_UNDERLINE = 1u << 3,
+    SGR_ATTR_BLINK     = 1u << 4,
+    SGR_ATTR_INVERSE   = 1u << 5,
+    SGR_ATTR_HIDDEN    = 1u << 6,
+    SGR_ATTR_STRIKE    = 1u << 7,
+} ansi_sgr_flag_t;
+
+typedef struct ansi_sgr_t {
+    uint32_t flags;
+    uint32_t fg_color;
+    uint32_t bg_color;
+} ansi_sgr_t;
+
+typedef struct ansi_csi_t {
+    bool dec_private;
+    char intermediates[5];
+    size_t intermediates_count;
+    int parameters[ANSI_MAX_PARAMETERS];
+    size_t parameters_count;
+    char final_byte;
+    ansi_csi_event_t event;
+    ansi_sgr_t attributes;
+    ansi_dec_mode_t dec_mode;
+} ansi_csi_t;
+
+typedef enum ansi_osc_event_t {
+    ANSI_OSC_KIND_UNKNOWN = 0,
+    ANSI_OSC_SET_TITLE,
+    ANSI_OSC_HYPERLINK,
+    ANSI_OSC_CLIPBOARD,
+} ansi_osc_event_t;
+
+typedef struct ansi_osc_t {
+    int code;
+    const char *payload;
+    ansi_osc_event_t event;
+} ansi_osc_t;
+
+typedef struct ansi_unknown_t {
+    const uint8_t *bytes;
+    size_t length;
+} ansi_unknown_t;
+
+typedef struct ansi_t {
+    ansi_event_t event;
+    union {
+        ansi_text_t text;
+        ansi_esc_t esc;
+        ansi_csi_t csi;
+        ansi_osc_t osc;
+        ansi_unknown_t unknown;
+    };
+} ansi_t;
+
+typedef enum ansi_color_t {
+    ANSI_COLOR_DEFAULT = 0,
+    ANSI_COLOR_INDEXED,
+    ANSI_COLOR_RGB,
+} ansi_color_t;
+
+uint32_t ansi_color_pack_indexed(int index);
+
+uint32_t ansi_color_pack_rgb(uint8_t red, uint8_t green, uint8_t blue);
+
+ansi_color_t ansi_color_unpack(uint32_t color, int *index, uint8_t *red, uint8_t *green, uint8_t *blue);
+
+#endif // !ANSI_H
