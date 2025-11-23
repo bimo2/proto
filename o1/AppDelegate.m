84849a6448aa1ffc121e8ff30aee85a605dee2ae
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2025-11-23 16:24:11 -0500

    delete storyboard + build window

--- o1/AppDelegate.m
+++ o1/AppDelegate.m
@@ -7,14 +7,32 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
+
 @interface AppDelegate ()
+
+@property (strong) NSWindow *window;
 
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    // TODO
+    NSRect frame = NSMakeRect(0, 0, 575, 375);
+    NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable | NSWindowStyleMaskFullSizeContentView;
+
+    _window = [[NSWindow alloc] initWithContentRect:frame styleMask:style backing:NSBackingStoreBuffered defer:NO];
+    _window.restorable = NO;
+    _window.titleVisibility = NSWindowTitleHidden;
+    _window.titlebarAppearsTransparent = YES;
+    _window.backgroundColor = [NSColor colorWithDeviceWhite:0.0 alpha:0.94];
+    _window.toolbar = [[NSToolbar alloc] init];;
+    _window.toolbar.displayMode = NSToolbarDisplayModeIconOnly;
+    _window.toolbarStyle = NSWindowToolbarStyleUnified;
+    _window.contentViewController = [[ViewController alloc] init];;
+    [_window setContentSize:frame.size];
+    [_window makeKeyAndOrderFront:nil];
+    [NSApp activateIgnoringOtherApps:YES];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
--- o1/main.m
+++ o1/main.m
@@ -5,12 +5,19 @@
 //  Created by grok-code-fast-1 on 2025-10-10.
 //
 
+#include "AppDelegate.h"
+
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // TODO
+        NSApplication *app = [NSApplication sharedApplication];
+        AppDelegate *delegate = [[AppDelegate alloc] init];
+
+        app.delegate = delegate;
+        [app setActivationPolicy:NSApplicationActivationPolicyRegular];
+        [app run];
     }
 
-    return NSApplicationMain(argc, argv);
+    return EXIT_SUCCESS;
 }
